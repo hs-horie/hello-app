cc3b6ee24367044fae44d532ce893349ed0f4482
hs_shibaoka.ryusei <shibaoka.ryusei@heartsoft.co.jp>
2024-09-13 15:52:17 +0900

    カバレッジのタスク名を修正

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,7 +54,7 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
-    task("jacocoTestReport", JacocoReport::class) {
+    task("coverageReport", JacocoReport::class) {
         dependsOn("testReleaseUnitTest")
         reports {
             xml.required = false
