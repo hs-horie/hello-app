2ea6611322e7163f0ee1d9b0a7934c8cbf473906
hs_arimoto.saki <arimoto.saki@heartsoft.co.jp>
2024-11-22 18:21:48 +0900

    ktlintの適用

--- app/src/main/java/com/example/helloApp/MainModel.kt
+++ app/src/main/java/com/example/helloApp/MainModel.kt
@@ -1,12 +1,8 @@
 package com.example.helloApp
 
-import kotlin.random.Random
-
 class MainModel {
     // テスト用 受け取った値が数字（Int）ならtrueを返す
-    fun isInt(input :Any):Boolean{
+    fun isInt(input: Any): Boolean {
         return input is Int
     }
-
-
 }
